f4ff561fd275126cb77e492ac86897b611acaec3
DomePuma <mathys.mechain@gmail.com>
2023-12-06 22:53:00 +0100

    Quick Sort + Tri à insertion

--- main.cpp
+++ main.cpp
@@ -1,18 +1,135 @@
 #include <iostream>
+#include <chrono>
+#include <random>
 
-void MinIndexInTab(int[] tab, int size, int indexInf,int indexSup)
+
+void TriParInsertion(int* tab, int size)
 {
-    for(int i = 0; i < size)
+    for (int i = 1; i < size; ++i) 
     {
-        
+        int aux = tab[i];
+        int j = i - 1;
+        while (j >= 0 && tab[j] > aux)
+        {
+            tab[j + 1] = tab[j];
+            j -= 1;
+        }
+        tab[j + 1] = aux;
+    }
+}
+
+void Swap(int* tab, int left, int right)
+{
+    int tabLeft = tab[left];
+    tab[left] = tab[right];
+    tab[right] = tabLeft;
+}
+
+int Partition(int* tab, int low, int high)
+{
+    int pivot_index = high;
+    int l = low;
+
+    for (int i = low; i < high; ++i) 
+    {
+        if (tab[i] <= tab[pivot_index])
+        {
+            Swap(tab, i, l);
+            l++;
+        }
+    }
+    Swap(tab, l, pivot_index);
+
+    return l;
+}
+
+void QuickShort(int* tab, int size, int low = 0, int high = 0)
+{
+
+    if (high == 0)
+    {
+        high = size - 1;
+    }
+
+    if (low < high)
+    {
+        int p = Partition(tab, low, high);
+
+        QuickShort(tab, p, low, p - 1);
+        QuickShort(tab, high + 1, p + 1, high);
+    }
+}
+
+void RandomArray(int* tab, int size)
+{
+
+    constexpr int MIN = 0;
+    constexpr int MAX = 9;
+
+    for (int i = 0; i < size; ++i) 
+    {
+        std::random_device rd;
+        std::default_random_engine eng(rd());
+        std::uniform_int_distribution<int> distr(MIN, MAX);
+
+        tab[i] = distr(eng);
     }
 }
 
 int main()
 {
-    int size = 3;
-    int tab[] = {8,9,6}
-    
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int tab1[] = {7, 1, 5, 2, 3};
+    int size1 = std::size(tab1);
+
+    QuickShort(tab1, size1);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Temps d'execution : " << duration.count() << " microsecondes." << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+
+    int tab2[100];
+    int size2 = std::size(tab2);
+
+    RandomArray(tab2, size2);
+
+    QuickShort(tab2, size2);
+
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Temps d'execution : " << duration.count() * .01 << " secondes." << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+
+    int tab3[1000];
+    int size3 = std::size(tab3);
+
+    RandomArray(tab3, size3);
+
+    QuickShort(tab3, size3);
+
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Temps d'execution : " << duration.count() * .01 << " secondes." << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+
+    int tab4[1000];
+    int size4 = std::size(tab4);
+
+    RandomArray(tab4, size4);
+
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Temps d'execution : " << duration.count() * .01 << " secondes." << std::endl;
+
 
     return 0;
 }
